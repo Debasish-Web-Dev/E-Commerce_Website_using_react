05b76db4ccce110f62ee1d3f824576e1fbcb11ef
Debasish-Web-Dev <146519258+Debasish-Web-Dev@users.noreply.github.com>
2024-04-24 20:39:32 +0530

    Add files via upload

---
+++ Debasish E-Commerce Website/src/pages/AboutPage.jsx
@@ -0,0 +1,59 @@
+import React from 'react'
+import { Footer, Navbar } from "../components";
+const AboutPage = () => {
+  return (
+    <>
+      <Navbar />
+      <div className="container my-3 py-3">
+        <h1 className="text-center">About Us</h1>
+        <hr />
+        <p className="lead text-center">
+          Our e-commerce platform is your one-stop destination for all your shopping needs. Featuring a diverse range of products meticulously curated for quality and variety, we cater to every taste and preference. From the latest gadgets and trendy fashion to household essentials and artisanal goods, our website offers an unparalleled shopping experience.
+
+          What sets us apart is our commitment to customer satisfaction. With intuitive navigation, secure payment gateways, and responsive customer support, we ensure a seamless and enjoyable shopping journey for every user. Our dedicated team works tirelessly to bring you the best deals, exclusive offers, and timely delivery, making your online shopping experience both convenient and rewarding.
+
+          Whether you're browsing for a specific item or simply seeking inspiration, our user-friendly interface and personalized recommendations make finding the perfect product a breeze. With our e-commerce website, you can shop with confidence, knowing that quality, affordability, and convenience are always at the forefront of what we do.
+        </p>
+
+        <h2 className="text-center py-4">Our Products</h2>
+        <div className="row">
+          <div className="col-md-3 col-sm-6 mb-3 px-3">
+            <div className="card h-100">
+              <img className="card-img-top img-fluid" src="https://images.pexels.com/photos/298863/pexels-photo-298863.jpeg?auto=compress&cs=tinysrgb&w=600" alt="" height={160} />
+              <div className="card-body">
+                <h5 className="card-title text-center">Mens's Clothing</h5>
+              </div>
+            </div>
+          </div>
+          <div className="col-md-3 col-sm-6 mb-3 px-3">
+            <div className="card h-100">
+              <img className="card-img-top img-fluid" src="https://images.pexels.com/photos/7679720/pexels-photo-7679720.jpeg?auto=compress&cs=tinysrgb&w=600" alt="" height={160} />
+              <div className="card-body">
+                <h5 className="card-title text-center">Women's Clothing</h5>
+              </div>
+            </div>
+          </div>
+          <div className="col-md-3 col-sm-6 mb-3 px-3">
+            <div className="card h-100">
+              <img className="card-img-top img-fluid" src="https://images.pexels.com/photos/1927259/pexels-photo-1927259.jpeg?auto=compress&cs=tinysrgb&w=600" alt="" height={160} />
+              <div className="card-body">
+                <h5 className="card-title text-center">Jewelery</h5>
+              </div>
+            </div>
+          </div>
+          <div className="col-md-3 col-sm-6 mb-3 px-3">
+            <div className="card h-100">
+              <img className="card-img-top img-fluid" src="https://images.pexels.com/photos/356056/pexels-photo-356056.jpeg?auto=compress&cs=tinysrgb&w=600" alt="" height={160} />
+              <div className="card-body">
+                <h5 className="card-title text-center">Electronics</h5>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <Footer />
+    </>
+  )
+}
+
+export default AboutPage
